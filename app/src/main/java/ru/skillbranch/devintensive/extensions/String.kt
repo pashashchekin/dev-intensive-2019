8e303fdba2889e205ebdea13b8737e14887e7c89
pashashchyokin <pashashchyokin@gmail.com>
2020-08-19 18:49:15 +0600

    init commit

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -0,0 +1,12 @@
+package ru.skillbranch.devintensive.extensions
+
+fun String.truncate(length: Int = 16): String {
+    var result = this.trim()
+    if (result.length > length) result = result.substring(0, length).trimEnd() + "..."
+    return result
+}
+
+
+fun String.stripHtml() = this
+        .replace(Regex("<[^>]*>|&amp;|&lt;|&gt;|&quot;|&apos;|&#\\d+;"), "")
+        .replace(Regex(" +"), " ")
---
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -0,0 +1,13 @@
+package ru.skillbranch.devintensive.utils
+
+object Utils {
+    fun parseFullName(fullName: String?): Pair<String?, String?> {
+        val parts: List<String>? = fullName?.split(" ")
+
+        val firstName = parts?.getOrNull(0)
+        val lastName = parts?.getOrNull(1)
+
+        return (if (firstName == "") null else firstName) to (if (lastName == "") null else lastName)
+
+    }
+}
