13419cbe9f5354481fff8b2ff145fa474a661fc6
pashashchyokin <pashashchyokin@gmail.com>
2020-08-20 12:42:14 +0600

    new build

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -4,10 +4,9 @@ object Utils {
     fun parseFullName(fullName: String?): Pair<String?, String?> {
         val parts: List<String>? = fullName?.split(" ")
 
-        val firstName = parts?.getOrNull(0)
-        val lastName = parts?.getOrNull(1)
+        val firstName = parts?.getOrNull(0)?.ifEmpty() { null }
+        val lastName = parts?.getOrNull(1)?.ifEmpty() { null }
 
-        return (if (firstName == "") null else firstName) to (if (lastName == "") null else lastName)
-
+        return firstName to lastName
     }
 }
